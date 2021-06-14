5231d654d2b70ff6ad48facbcb9da7319f713788
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-14 03:03:02 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Installing..."
+sleep 2
+sudo apt install python
+sudo apt install python3
+chmod +x location.py
+echo ""
+echo "Finish!"
+echo ""
+exit 1
